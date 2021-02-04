0d85057188c1aa7884db7c8a81936da010689857
Theo <theojherve@gmail.com>
2021-02-04 21:57:11 +0100

    SendFileInServer Working !
    needs to be adapted to other applications

--- Application C/sendFileInServer/main.c
+++ Application C/sendFileInServer/main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <curl.h>
-#include <MYSQL/mysql.h>
 #include <string.h>
 
-size_t readFunction(char *contents, size_t size, size_t nmemb, void *userp){
-  FILE *readhere = (FILE *)userp;
-  curl_off_t nread;
+#include <sys/types.h>
+#include <sys/stat.h>
 
-  /* copy as much data as possible into the 'contents' buffer, but no more than
-     'size' * 'nmemb' bytes! */
-  size_t retcode = fread(contents, size, nmemb, readhere);
+#include <curl.h>
+#include <MYSQL/mysql.h>
+
+static size_t read_callback(char *ptr, size_t size, size_t nmemb, void *stream){
+  curl_off_t nread;
+  /* in real-world cases, this would probably get this data differently
+     as this fread() stuff is exactly what the library already would do
+     by default internally */
+  size_t retcode = fread(ptr, size, nmemb, stream);
 
   nread = (curl_off_t)retcode;
 
@@ -21,9 +24,6 @@ size_t readFunction(char *contents, size_t size, size_t nmemb, void *userp){
 
 void sendFileInServer(char* idUser){
 
-//    MYSQL_RES *result=NULL;
-//    MYSQL_ROW row;
-
     MYSQL mysql;
     mysql_init(&mysql);
     mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"option");
@@ -31,73 +31,72 @@ void sendFileInServer(char* idUser){
     if(mysql_real_connect(&mysql,"51.77.144.219","theo","jaimelethe","LTA",0,NULL,0)){
 
         CURL *curl;
-        int result;
-        char errbuf[CURL_ERROR_SIZE];
-
-        FILE *fp = NULL;
-
-        fp = fopen("file/DownloadedFile.txt", "rb");
+        CURLcode result;
+        struct stat file_info;
+        curl_off_t speed_upload, total_time;
 
         char query[255];
 
-        //System("curl -u name:passwd ftp://machine.domain:port/full/path/to/file")
+        FILE *fp;
 
-        curl = curl_easy_init(); //initialize CURL fonction
+        fp = fopen("file/downloadedFile.txt", "rb"); /* open file to upload */
+        if(!fp)
+        return 1; /* can't continue */
 
+        /* to get the file size */
+        if(fstat(fileno(fp), &file_info) != 0)
+        return 1; /* can't continue */
+
+        curl = curl_easy_init();
         if(curl){
 
-            //curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-
-            curl_off_t uploadsize = -1;
-
-//            strcpy(query, "https://www.lta-development.fr/users/");
-            strcpy(query, "ftp://lta-development.fr/users/");
+            strcpy(query, "sftp://51.77.144.219/var/www/html/users/");
             strcat(query, idUser);
-            strcat(query, "/");
+            strcat(query, "/downloadedFile.txt");
 
-            printf("query = %s\n\n", query);
+            printf("query : %s\n", query);
 
-            curl_easy_setopt(curl, CURLOPT_URL, query); //CURLOPT_ULR allow us to enter the url of the file we want to dl
+            curl_easy_setopt(curl, CURLOPT_USERPWD, "root:n73r96uxZbfC");
 
+            /* upload to this place */
+            curl_easy_setopt(curl, CURLOPT_URL, query);
 
-            size_t function(char *bufptr, size_t size, size_t nitems, void *userp); //maybe not necessary
+            curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
 
-            curl_easy_setopt(curl, CURLOPT_READFUNCTION, readFunction);    //maybe not necessary
+            /* tell it to "upload" to the URL */
+            curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
 
-            curl_easy_setopt(curl, CURLOPT_READDATA, (void *)fp);    ////maybe not necessary
+            /* set where to read from (on Windows you need to use READFUNCTION too) */
+            curl_easy_setopt(curl, CURLOPT_READDATA, fp);
 
+            /* and give the size of the upload (optional) */
+            curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE,
+                             (curl_off_t)file_info.st_size);
 
-            curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L); //tell libcurl that we want to upload
+            /* enable verbose for easier tracing */
+            curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
-            curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, uploadsize);
-
-
-
-
-            //curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp); //alow to write on the file we dl
-            curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
-            curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
-            errbuf[0] = 0;
-            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-
-            curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);//enable progress meter
-
-            result = curl_easy_perform(curl); //return if the dl was successful (might take few seconds)
-
+            result = curl_easy_perform(curl);
+            /* Check for errors */
             if(result != CURLE_OK) {
-                size_t len = strlen(errbuf);
-                fprintf(stderr, "\nlibcurl: (%d) ", result);
-                if(len){
-                    fprintf(stderr, "%s%s", errbuf, ((errbuf[len - 1] != '\n') ? "\n" : ""));
-                }
-            }else{
-                    fprintf(stderr, "\n%s\n", curl_easy_strerror(result));
-                    printf("Download successful !\n");
-                }
+              fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                      curl_easy_strerror(result));
 
-            fclose(fp);
+            }else{
+              /* now extract transfer info */
+              curl_easy_getinfo(curl, CURLINFO_SPEED_UPLOAD_T, &speed_upload);
+              curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME_T, &total_time);
+
+              fprintf(stderr, "Speed: %" CURL_FORMAT_CURL_OFF_T " bytes/sec during %"
+                      CURL_FORMAT_CURL_OFF_T ".%06ld seconds\n",
+                      speed_upload,
+                      (total_time / 1000000), (long)(total_time % 1000000));
+
+            }
+            /* always cleanup */
             curl_easy_cleanup(curl);
         }
+        fclose(fp);
     }
 
 }
@@ -106,10 +105,10 @@ int main(int argc, char **argv){
 
     int idUser = 2;
     char* idUserChar;
-
     idUserChar = malloc(sizeof(char)*3+1);
 
     sendFileInServer(itoa(idUser,  idUserChar, 10));
 
     return 0;
 }
+
